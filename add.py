1337c0351b4f0b8afb3f76b11badfe78bfa3838f
suvamcuelogic <suvam.chowdhury@cuelogic.com>
2021-06-10 19:42:13 +0530

    add method added

---
+++ add.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    try:
+        return int(a)+ int(b)
+    except ValueError:
+        return "Enter valid Numeric value"
+    
+
+x= input(" enter first Number")
+y= input(" enter second Number")
+
+print(add(x,y))
